53bf032c1569d2f05d41ddd1132af6f7f503c30a
oreol Noumodong <oreolnoumodong@gmail.com>
2021-02-08 22:08:21 +0100

    Add ratings

--- assets/js/Restaurant.js
+++ assets/js/Restaurant.js
@@ -4,6 +4,7 @@ class Restaurant {
         this.address = address;
         this.position = position;
         this.ratings = ratings;
+        this.avgRating = this.calcAverage();;
         this.comments = this.fillComments();
         this.index = index;
     }
@@ -14,7 +15,7 @@ class Restaurant {
                             <div class="col-12 make-shbr mt-3 animate__animated animate__fadeInRight animate__delay-0.5s">
                                     <p class="mt-3 mb-3"><strong>${this.name}</strong></p>
                                     <p class="my-3">${this.address}</p>
-                                    <p class="my-3">Raiting</p>
+                                    <p class="my-3">Raiting: <strong>${this.avgRating}</strong></p>
                                     <p>
                                     <div class="accordion" id="accordionExample">
                                         <div class="card">
@@ -50,5 +51,25 @@ class Restaurant {
         })
 
         return comments;
+    }
+
+    calcAverage() {
+        let totalRatings = 0;
+        let averageRating = 0;
+
+
+        if (this.ratings.length === 0) {
+            return 0;
+        }
+        this.ratings.forEach(item => {
+            totalRatings += item.stars;
+        })
+
+        averageRating = totalRatings / this.ratings.length;
+        averageRating = Math.round(averageRating * 10) / 10;
+
+        console.log(averageRating)
+
+        return averageRating;
     }
 }
